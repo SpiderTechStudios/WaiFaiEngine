dd967e612c88c9292ca00b7bb1b7f4f60955df64
juku001 <jumakassim89@gmail.com>
2026-09-20 12:45:58 +0300

    feat:remove zali

--- app/Http/Controllers/TestWifiDogController.php
+++ app/Http/Controllers/TestWifiDogController.php
@@ -52,8 +52,7 @@ class TestWifiDogController extends Controller
         if ($url !== '') {
             $query .= '&url='.rawurlencode($url);
         }
-
-        $gwAddress = '192.168.0.1';
+        
         $gatewayAuthUrl = "http://{$gwAddress}:{$gwPort}/wifidog/auth?{$query}";
 
         Log::info('WIFIDOG LOGIN -> GATEWAY AUTH', [
