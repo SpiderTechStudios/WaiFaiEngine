50f8a16c3b35cf479253ebf99e67baad20c9796b
juku001 <jumakassim89@gmail.com>
2026-09-21 19:40:31 +0800

    back to wifidog

--- routes/Api/wifidog.php
+++ routes/Api/wifidog.php
@@ -38,7 +38,7 @@ use PHPUnit\Framework\Attributes\Group;
 
 
 
-Route::prefix('Wifidog')->group(function () {
+Route::prefix('wifidog')->group(function () {
     Route::match(['get', 'post'], '/login', [TestingWifidogController::class, 'login']);
     Route::match(['get', 'post'], '/auth', [TestingWifidogController::class, 'auth']);
     Route::match(['get', 'post'], '/portal', [TestingWifidogController::class, 'portal']);
